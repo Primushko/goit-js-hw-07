cdab1ae806440c0051bdb629761eb60ef94c5193
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-03-23 14:40:00 +0200

    ДЗ 7.1 створив розмітку галереї

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const galleryItemsMarkup = galleryItems
+    .map(
+    ({ preview, original, description }) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+    />
+    </a>
+    </li>`
+    )
+    .join("");
+
+    galleryList.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+    
+
+
