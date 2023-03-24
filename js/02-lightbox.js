c87783f2491d7bc23ec77ac7163aec96f01f311f
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-03-24 10:58:55 +0200

    ДЗ 7.2. початок

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,54 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryItemsMarkup = galleryItems
+    .map(
+    ({ preview, original, description }) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </li>`
+    ).join("");
+
+// gallery.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+// gallery.addEventListener(`click`, onGalleryClick);
+
+// function onGalleryClick(event) {
+
+    // event.preventDefault();
+    
+//   const target = event.target;
+//   if (target.nodeName !== `IMG`) {
+//     return;
+//   }
+    
+//   const instance = basicLightbox.create(
+//     `<img src="${target.dataset.source}" width="800" height="600">`,
+//     {
+//       onShow: () => {
+//         document.addEventListener(`keydown`, onEscapePress);
+//       },
+//       onClose: () => {
+//         document.removeEventListener(`keydown`, onEscapePress);
+//       },
+//     }
+//   );
+
+//   instance.show();
+    
+//   function onEscapePress(event) {
+//     if (event.code === `Escape`) {
+//       instance.close();
+//     }
+//   }
+  // console.log(event.target);
+// } 
